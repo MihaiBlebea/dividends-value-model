4961470293d3334ec9057345f8a071637d128a93
Mihai Blebea <mihai@getchip.uk>
2022-12-26 21:28:41 +0000

    Adds more endpoints

--- api/main.py
+++ api/main.py
@@ -1,143 +1,164 @@
 from flask import Flask, jsonify, request
 from src.dividend_indicator import DividendIndicator
 from src.dividend_calculator import DividendCalculator
+from src.ticker import Ticker
 
 
 app = Flask(__name__)
 
 API_V1 = "/api/v1"
 
+
 @app.route("/", methods=["GET"])
 def index():
-	try:
-		host = request.host_url.rstrip("/")
-		return jsonify({
-			"status": "OK",
-			"data": [
-				f"{host}{API_V1}/dividends/AAPL/future-earnings",
-				f"{host}{API_V1}/dividends/AAPL/dividends-per-month",
-				f"{host}{API_V1}/dividends/AAPL/years-till-recoup",
-				f"{host}{API_V1}/dividends/AAPL/historic-per-year",
-				f"{host}{API_V1}/dividends/AAPL/dividends-for-investment",
-				f"{host}{API_V1}/dividends/AAPL/indicators"
-			]
-		}), 200
-	except Exception as err:
-		return jsonify({
-			"status": "ERROR",
-			"error": str(err)
-		}), 500
+    try:
+        host = request.host_url.rstrip("/")
+        return (
+            jsonify(
+                {
+                    "status": "OK",
+                    "data": [
+                        f"{host}{API_V1}/dividends/AAPL/future-earnings",
+                        f"{host}{API_V1}/dividends/AAPL/dividends-per-month",
+                        f"{host}{API_V1}/dividends/AAPL/years-till-recoup",
+                        f"{host}{API_V1}/dividends/AAPL/historic-per-year",
+                        f"{host}{API_V1}/dividends/AAPL/dividends-for-investment",
+                        f"{host}{API_V1}/dividends/AAPL/indicators",
+                        f"{host}{API_V1}/dividends/AAPL/company-basics",
+                    ],
+                }
+            ),
+            200,
+        )
+    except Exception as err:
+        return jsonify({"status": "ERROR", "error": str(err)}), 500
+
 
 @app.route(f"{API_V1}/dividends/<symbol>/future-earnings", methods=["GET"])
 def get_future_earnings(symbol: str):
-	try:
-		args = request.args
-		invest_amount = args.get("invest-amount", type=float, default=20_000)
-		years = args.get("years", type=int, default=10)
-		reinvest_dividends = args.get("reinvest-dividends", type=bool, default=True)
-		reinvest_annualy = args.get("reinvest-annualy", type=bool, default=True)
+    try:
+        args = request.args
+        invest_amount = args.get("invest-amount", type=float, default=20_000)
+        years = args.get("years", type=int, default=10)
+        reinvest_dividends = args.get("reinvest-dividends", type=bool, default=True)
+        reinvest_annualy = args.get("reinvest-annualy", type=bool, default=True)
 
-		dc = DividendCalculator(symbol)
-		result = dc.predict_future_earnings(
-			invest_amount,
-			years, 
-			reinvest_dividends, 
-			reinvest_annualy,
-		)
+        dc = DividendCalculator(symbol)
+        result = dc.predict_future_earnings(
+            invest_amount,
+            years,
+            reinvest_dividends,
+            reinvest_annualy,
+        )
 
-		return jsonify({
-			"status": "OK",
-			"data": result
-		}), 200
-	except Exception as err:
-		return jsonify({
-			"status": "ERROR",
-			"error": str(err)
-		}), 500
+        return jsonify({"status": "OK", "data": result}), 200
+    except Exception as err:
+        return jsonify({"status": "ERROR", "error": str(err)}), 500
+
 
 @app.route(f"{API_V1}/dividends/<symbol>/dividends-per-month", methods=["GET"])
 def get_invest_for_dividend_per_month(symbol: str):
-	try:
-		args = request.args
-		dividend_per_month = args.get("dividend-per-month", type=float, default=1_000)
-		dc = DividendCalculator(symbol)
-		return jsonify({
-			"status": "OK",
-			"data": dc.invest_for_dividend_per_month(dividend_per_month)
-		}), 200
-	except Exception as err:
-		return jsonify({
-			"status": "ERROR",
-			"error": str(err)
-		}), 500
+    try:
+        args = request.args
+        dividend_per_month = args.get("dividend-per-month", type=float, default=1_000)
+        dc = DividendCalculator(symbol)
+        return (
+            jsonify(
+                {
+                    "status": "OK",
+                    "data": dc.invest_for_dividend_per_month(dividend_per_month),
+                }
+            ),
+            200,
+        )
+    except Exception as err:
+        return jsonify({"status": "ERROR", "error": str(err)}), 500
+
 
 @app.route(f"{API_V1}/dividends/<symbol>/years-till-recoup", methods=["GET"])
 def get_years_till_recoup_investment(symbol: str):
-	try:
-		dc = DividendCalculator(symbol)
-		return jsonify({
-			"status": "OK",
-			"data": dc.years_till_recoup_investment()
-		}), 200
-	except Exception as err:
-		return jsonify({
-			"status": "ERROR",
-			"error": str(err)
-		}), 500
+    try:
+        dc = DividendCalculator(symbol)
+        return jsonify({"status": "OK", "data": dc.years_till_recoup_investment()}), 200
+    except Exception as err:
+        return jsonify({"status": "ERROR", "error": str(err)}), 500
+
 
 @app.route(f"{API_V1}/dividends/<symbol>/historic-per-year", methods=["GET"])
 def get_historic_per_year(symbol: str):
-	try:
-		dc = DividendCalculator(symbol)
-		return jsonify({
-			"status": "OK",
-			"data": dc.get_dividends_per_year()
-		}), 200
-	except Exception as err:
-		return jsonify({
-			"status": "ERROR",
-			"error": str(err)
-		}), 500
+    try:
+        dc = DividendCalculator(symbol)
+        return jsonify({"status": "OK", "data": dc.get_dividends_per_year()}), 200
+    except Exception as err:
+        return jsonify({"status": "ERROR", "error": str(err)}), 500
+
 
 @app.route(f"{API_V1}/dividends/<symbol>/dividends-for-investment", methods=["GET"])
 def get_dividends_for_investment(symbol: str):
-	try:
-		args = request.args
-		invest_amount = args.get("invest-amount", type=float, default=20_000)
+    try:
+        args = request.args
+        invest_amount = args.get("invest-amount", type=float, default=20_000)
 
-		dc = DividendCalculator(symbol)
-		return jsonify({
-			"status": "OK",
-			"data": dc.dividends_for_amount_invested(invest_amount)
-		}), 200
-	except Exception as err:
-		return jsonify({
-			"status": "ERROR",
-			"error": str(err)
-		}), 500
+        dc = DividendCalculator(symbol)
+        return (
+            jsonify(
+                {
+                    "status": "OK",
+                    "data": dc.dividends_for_amount_invested(invest_amount),
+                }
+            ),
+            200,
+        )
+    except Exception as err:
+        return jsonify({"status": "ERROR", "error": str(err)}), 500
+
 
 @app.route(f"{API_V1}/dividends/<symbol>/indicators", methods=["GET"])
 def get_indicators(symbol: str):
-	try:
-		dc = DividendCalculator(symbol)
-		di = DividendIndicator(symbol)
-		return jsonify({
-			"status": "OK",
-			"data": {
-				"dividend_yield": di.get_dividend_yield(),
-				"current_price": di.get_current_price(),
-				"current_dividend_amount": dc.current_year_div_per_share(),
-				"dividend_growth": dc.get_yearly_dividend_growth(5),
-				"dividend_ratios_per_year": di.get_yearly_ratios(),
-				"cadi": dc.get_cadi(),
-			}
-		}), 200
-	except Exception as err:
-		return jsonify({
-			"status": "ERROR",
-			"error": str(err)
-		}), 500
+    try:
+        dc = DividendCalculator(symbol)
+        di = DividendIndicator(symbol)
+        return (
+            jsonify(
+                {
+                    "status": "OK",
+                    "data": {
+                        "dividend_yield": di.get_dividend_yield(),
+                        "current_price": di.get_current_price(),
+                        "current_dividend_amount": dc.current_year_div_per_share(),
+                        "dividend_growth": dc.get_yearly_dividend_growth(5),
+                        "dividend_ratios_per_year": di.get_yearly_ratios(),
+                        "cadi": dc.get_cadi(),
+                        "beta": di.get_beta(),
+                    },
+                }
+            ),
+            200,
+        )
+    except Exception as err:
+        return jsonify({"status": "ERROR", "error": str(err)}), 500
+
+
+@app.route(f"{API_V1}/dividends/<symbol>/company-basics", methods=["GET"])
+def get_indicators(symbol: str):
+    try:
+        t = Ticker(symbol)
+        return (
+            jsonify(
+                {
+                    "status": "OK",
+                    "data": {
+                        "company_name": t.get_company_name(),
+                        "industry": t.get_industry(),
+                        "sector": t.get_sector(),
+                    },
+                }
+            ),
+            200,
+        )
+    except Exception as err:
+        return jsonify({"status": "ERROR", "error": str(err)}), 500
 
 
 if __name__ == "__main__":
-	app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
--- src/utils.py
+++ src/utils.py
@@ -12,89 +12,94 @@ cc = CurrencyConverter(SINGLE_DAY_ECB_URL)
 
 DETA_DRIVER_NAME = "dividend-calculator"
 
-def safeget(dct: dict, *keys):
-	for key in keys:
-		try:
-			dct = dct[key]
-		except KeyError:
-			return None
-	return dct
 
-def calc_percentage_diff(initial: float, current: float)-> float:
-	return abs(initial - current) / current
+def safeget(dct: dict, *keys):
+    for key in keys:
+        try:
+            dct = dct[key]
+        except KeyError:
+            return None
+    return dct
+
+
+def calc_percentage_diff(initial: float, current: float) -> float:
+    return abs(initial - current) / current
+
 
 def cache_factory(cache_dir: str, file_prefix: str, ttl_sec: int):
-	def cache(func):
-		def wrapper(*args, **kwargs):
+    def cache(func):
+        def wrapper(*args, **kwargs):
 
-			args_params = [str(argv) for argv in args[1:]]
-			kwargs_params = [str(argv) for argv in list(kwargs.values())]
+            args_params = [str(argv) for argv in args[1:]]
+            kwargs_params = [str(argv) for argv in list(kwargs.values())]
 
-			file_sufix = "_".join(args_params + kwargs_params)
+            file_sufix = "_".join(args_params + kwargs_params)
 
-			Path(cache_dir).mkdir(parents=True, exist_ok=True)
+            Path(cache_dir).mkdir(parents=True, exist_ok=True)
 
-			cache_file_path = Path(f"{cache_dir}/{file_prefix}_{file_sufix}.json")
+            cache_file_path = Path(f"{cache_dir}/{file_prefix}_{file_sufix}.json")
 
-			# Check if this is running on Deta
-			project_key = os.getenv("DETA_PROJECT_KEY")
-			deta_file = f"{file_prefix}_{file_sufix}.json"
-			if project_key is not None:
-				drive = Drive(DETA_DRIVER_NAME)
-				file = drive.get(deta_file)
-				if file is not None:
-					return json.loads(file.read())
+            # Check if this is running on Deta
+            project_key = os.getenv("DETA_PROJECT_KEY")
+            deta_file = f"{file_prefix}_{file_sufix}.json"
+            if project_key is not None:
+                drive = Drive(DETA_DRIVER_NAME)
+                file = drive.get(deta_file)
+                if file is not None:
+                    return json.loads(file.read())
 
-			if cache_file_path.is_file():
-				
-				now = int(time.time())
-				created_at = int(cache_file_path.stat().st_mtime)
+            if cache_file_path.is_file():
 
-				if now < created_at + ttl_sec:
-					# print("Get data from cache")
-					with open(cache_file_path, "r") as file:
-						return json.loads(file.read())
+                now = int(time.time())
+                created_at = int(cache_file_path.stat().st_mtime)
 
-			# print("Get data from source")
-			result = func(*args, **kwargs)
+                if now < created_at + ttl_sec:
+                    # print("Get data from cache")
+                    with open(cache_file_path, "r") as file:
+                        return json.loads(file.read())
 
-			# Check if this is running on Deta
-			if project_key is not None:
-				drive = Drive(DETA_DRIVER_NAME)
-				drive.put(deta_file, data=json.dumps(result))
+            # print("Get data from source")
+            result = func(*args, **kwargs)
 
-				return result
+            # Check if this is running on Deta
+            if project_key is not None:
+                drive = Drive(DETA_DRIVER_NAME)
+                drive.put(deta_file, data=json.dumps(result))
 
-			with open(cache_file_path, "w") as file:
-				file.write(json.dumps(result, indent=4, ensure_ascii=False))
+                return result
 
-			return result
-		return wrapper
-	return cache
+            with open(cache_file_path, "w") as file:
+                file.write(json.dumps(result, indent=4, ensure_ascii=False))
+
+            return result
+
+        return wrapper
+
+    return cache
 
 
 def clear_deta_cache():
-	drive = Drive(DETA_DRIVER_NAME)
+    drive = Drive(DETA_DRIVER_NAME)
 
-	files = drive.list()
-	if files is None:
-		return
+    files = drive.list()
+    if files is None:
+        return
 
-	drive.delete_many(files["names"])
-	print("deleted all files in cache")
+    drive.delete_many(files["names"])
+    print("deleted all files in cache")
 
 
-def growth_in_percentage(data: List[float])-> List[float]:
-	initial = data[0]
-	res = [0]
-	for d in data[1:]:
-		res.append(calc_percentage_diff(initial, d))
+def growth_in_percentage(data: List[float]) -> List[float]:
+    initial = data[0]
+    res = [0]
+    for d in data[1:]:
+        res.append(calc_percentage_diff(initial, d))
 
-	return res
+    return res
 
-def to_GBP(amount: float, from_currency: str)-> float:
-	if from_currency == "GBp":
-		return amount / 100
 
-	return cc.convert(amount, from_currency, "GBP")
+def to_GBP(amount: float, from_currency: str) -> float:
+    if from_currency == "GBp":
+        return amount / 100
 
+    return cc.convert(amount, from_currency, "GBP")
